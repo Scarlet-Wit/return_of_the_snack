8c389dc11a11470fc2730ec42407f6760ac4d7f9
Masha-L <lifsh22m@mtholyoke.edu>
2021-01-29 00:35:24 -0500

    Updates

---
+++ precache-manifest.458bafabcccc8d864f13bf4b9e0d2669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e3d234f20b9eb9aef17385dec69bc7",
+    "url": "/return_of_the_snack/index.html"
+  },
+  {
+    "revision": "26d6bfa243c7f6c43c2a",
+    "url": "/return_of_the_snack/static/css/main.31dbc5f9.chunk.css"
+  },
+  {
+    "revision": "c5b590341965c3481e7b",
+    "url": "/return_of_the_snack/static/js/2.7d48469f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/return_of_the_snack/static/js/2.7d48469f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d6bfa243c7f6c43c2a",
+    "url": "/return_of_the_snack/static/js/main.c684382a.chunk.js"
+  },
+  {
+    "revision": "f5f1c53cc395c822ca3d",
+    "url": "/return_of_the_snack/static/js/runtime-main.c8ab359c.js"
+  }
+]);
