0c2eef1918822befcaad42a42b2febe01f588571
Masha-L <lifsh22m@mtholyoke.edu>
2021-01-29 16:33:29 -0500

    everything

---
+++ build/precache-manifest.2263997ea99d7f5e3e0e5904ab9a2604.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf51ba1f05e9e68a550a1e9451e6a71",
+    "url": "/return_of_the_snack/index.html"
+  },
+  {
+    "revision": "96bf97123ec544049a51",
+    "url": "/return_of_the_snack/static/css/main.f554b599.chunk.css"
+  },
+  {
+    "revision": "0ee0dc006e1dc0c3091f",
+    "url": "/return_of_the_snack/static/js/2.84638598.chunk.js"
+  },
+  {
+    "revision": "a8708c1560a44a3a2f75343bf0ed33a1",
+    "url": "/return_of_the_snack/static/js/2.84638598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bf97123ec544049a51",
+    "url": "/return_of_the_snack/static/js/main.a15c31b0.chunk.js"
+  },
+  {
+    "revision": "f5f1c53cc395c822ca3d",
+    "url": "/return_of_the_snack/static/js/runtime-main.c8ab359c.js"
+  },
+  {
+    "revision": "2acefb652115c14f0f4e35d415a1ad7b",
+    "url": "/return_of_the_snack/static/media/background.2acefb65.png"
+  }
+]);
