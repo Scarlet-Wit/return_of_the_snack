6ba3020a987ece27c5bf4d08a7b80808a611b617
Masha-L <lifsh22m@mtholyoke.edu>
2021-01-29 16:35:33 -0500

    Updates

--- precache-manifest.a639ef828be0e218c9e071d1215af3d9.js
+++ precache-manifest.a639ef828be0e218c9e071d1215af3d9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bcf51ba1f05e9e68a550a1e9451e6a71",
+    "revision": "fc52fd2e915d4f9f7455a0593e4ada83",
     "url": "/return_of_the_snack/index.html"
   },
   {
-    "revision": "96bf97123ec544049a51",
-    "url": "/return_of_the_snack/static/css/main.f554b599.chunk.css"
+    "revision": "229637ce70956fa584cc",
+    "url": "/return_of_the_snack/static/css/main.a6425dd0.chunk.css"
   },
   {
     "revision": "0ee0dc006e1dc0c3091f",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/return_of_the_snack/static/js/2.84638598.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "96bf97123ec544049a51",
+    "revision": "229637ce70956fa584cc",
     "url": "/return_of_the_snack/static/js/main.a15c31b0.chunk.js"
   },
   {
